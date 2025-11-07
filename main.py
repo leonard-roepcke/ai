2c9a7c37cbd6ea3f3810fbafeb43cae313658a76
leonard-roepcke <leonard.roepcke.2@gmail.com>
2025-11-07 18:31:55 +0100

    Matrix

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ import numpy as np
 print("Ai Initiated")
 test_array = np.array([1,2,3])
 print(test_array)
+test_matrix = np.array([
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+])
+print(test_matrix)
+print(test_matrix * 2)
